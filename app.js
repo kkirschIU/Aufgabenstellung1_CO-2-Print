1312eddc961cbbf69123a18bd9271bdf752c1289
kkirschIU <kevin.kirsch@iubh-fernstudium.de>
2023-08-29 15:15:41 +0200

    index_mit_vue --> alle Funktionen in vue code über führt

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
-new Vue({
-    el: '#app',
+  new Vue({
+    el: '#countries_CO2',
     data: {
-      isRtlLanguage: false,
-      lang: navigator.language || navigator.userLanguage,
-      rtlLanguages: ["ar", "he", "fa", "ur"]
-    },
-    methods: {
-      setContainerOrder() {
-        this.isRtlLanguage = this.rtlLanguages.includes(this.lang.substr(0, 2));
-        console.log("Erkannte Browsersprache:", this.lang);
-      }
+      countries: [],
+      filteredCountries: [],
+      countrySearchTerm: '',
+      sortOption: 'default'
     },
     mounted() {
-      this.setContainerOrder();
+      fetch('countries.json')
+        .then(response => response.json())
+        .then(data => {
+          this.countries = data.countries;
+          this.filteredCountries = data.countries;
+        })
+        .catch(error => {
+          console.error('Fehler beim Laden der Daten:', error);
+        });
+    },
+    methods: {
+      filterCountries() {
+        const searchTerm = this.countrySearchTerm.toLowerCase();
+        this.filteredCountries = this.countries.filter(country => country.Land.toLowerCase().includes(searchTerm));
+      },
+      sortTableCountry() {
+        const rows = this.filteredCountries.slice(0); // Kopie der gefilterten Länder
+  
+        if (this.sortOption === 'alphabetical') {
+          rows.sort((a, b) => a.Land.localeCompare(b.Land));
+        } else if (this.sortOption === 'emissions_asc') {
+          rows.sort((a, b) => a['tCO2/Kopf'] - b['tCO2/Kopf']);
+        } else if (this.sortOption === 'emissions_desc') {
+          rows.sort((a, b) => b['tCO2/Kopf'] - a['tCO2/Kopf']);
+        }
+  
+        this.filteredCountries = rows;
+      }
+    },
+    watch: {
+      countrySearchTerm() {
+        this.filterCountries();
+      },
+      sortOption() {
+        this.sortTableCountry();
+      }
     }
   });
+
+
+  
+  new Vue({
+    el: '#rtl',
+    data: {
+      isRtlLanguage: false
+    },
+    mounted() {
+      const lang = window.navigator.language || window.navigator.userLanguage;
+      this.isRtlLanguage = ["ar", "he", "fa", "ur"].includes(lang.substr(0, 2));
+    }
+  });
+  
